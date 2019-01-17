09dd04182b8ae56bfaba73b9ff37435b7e3fd2dd
Stevens-Matt <43816256+Stevens-Matt@users.noreply.github.com>
2019-01-17 14:17:11 -0500

    Prints out basics lines of text using C++.

---
+++ M1HW1_Stevens/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+// CSC 134
+// M1HW1
+// Matthew Stevens
+// 01/17/2019
+// This program prints outs basic lines of text.
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, my name is Matthew Stevens! " << endl;
+    cout << "I have been attending FTCC for " << 2 << " years " << endl;
+    cout << "I have " << 0 << " pets" << endl;
+    cout << "I am in the Associate of Science program" << endl;
+    cout << "My focus is in computer programming" << endl;
+    cout << "My preferred operating system is PC" << endl;
+    return 0;
+}
