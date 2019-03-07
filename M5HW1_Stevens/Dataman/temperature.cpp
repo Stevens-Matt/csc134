c9eb4004fbb2e91d815fdbf520cd775c13a58a00
Stevens-Matt <43816256+Stevens-Matt@users.noreply.github.com>
2019-03-07 14:54:13 -0500

    Silver submission of Dataman
    Bronze (max 85/100): 
    
    Go through the existing program and test it.
    
    (First, trade off with a classmate and try to use each others program. Next, show it to someone you know who doesn't write programs such as a friend or family member, and ask them to try it out. Are they able to navigate the menus easily? Do they get lost or stuck?)
    
    Based on your tests, and feedback from users, go through and add "quality of life" improvements to the program. For example, all the menus should have a consistent look and feel. 
    
    In addition, make sure that all code related to math is in the arithmetic source file, and all code related to temperature conversion is in the temperature source file.
    
    Silver (max 95/100): 
    
    Bronze, plus add Celcius -> Fahrenheit and F -> C functionality to the temperature converter.

--- M5HW1_Stevens/Dataman/temperature.cpp
+++ M5HW1_Stevens/Dataman/temperature.cpp
@@ -26,11 +26,29 @@ float convertCtoK(float tempC)
     return tempK;
 }
 
+float convertFtoC(float tempF)
+{
+    float tempC;
+
+    // Convert
+    tempC = (tempF - 32)*(5/9);
+    return tempC;
+}
+
+float convertCtoF(float tempC)
+{
+    float tempF;
+
+    // Convert
+    tempF = (tempC *(9/5)) + 32;
+    return tempF;
+}
 void tempConvertMenu()  // Contains the menu for the temp conversion function
 {
     int choice;
     float tempK;         // Temperature in Kelvin
     float tempC;         // Temperature in Celsius
+    float tempF;         // Temperature in Fahrenheit
 
     do
     {
@@ -39,27 +57,39 @@ void tempConvertMenu()  // Contains the menu for the temp conversion function
         cout << "----TEMPERATURE CONVERSION MENU----" << endl;
         cout << "1: Celsius to Kelvin " << endl;
         cout << "2: Kelvin to Celsius " << endl;
+        cout << "3: Fahrenheit to Celsius " << endl;
+        cout << "4: Celsius to Fahrenheit" << endl;
         cout << "0: Return to the main menu. " << endl;
         cin >> choice;
         switch (choice)
         {
         case 1:
             // Allow for the conversion to take place
-            cout << "-Celsius to Kelvin converter- " << endl;
+            cout << "-Celsius to Kelvin Converter- " << endl;
             // Get the temperature in Celsius from the user
             cout << "Enter the temperature in Celsius: " << endl;
             cin >> tempC;
             cout << "The temperature in Kelvin is: " << convertCtoK(tempC) << endl;
             break;
         case 2:
-            cout << "-Kelvin to Celsius converter- " << endl;      // Add the function KtoC
+            cout << "-Kelvin to Celsius Converter- " << endl;      // Add the function KtoC
             // Get the temperature in Kelvin from the user
             cout << "Enter the temperature in Kelvin: " << endl;
             cin >> tempK;
             cout << "The temperature in Celsius is: " << convertKtoC(tempK) << endl;
             break;
+        case 3:
+            cout << "-Fahrenheit to Celsius Converter-" << endl;
+            cout << "Enter the temperature in Fahrenheit: " << endl;
+            cin >> tempF;
+            cout << "The temperature in Celsius is: " << convertFtoC(tempF) << endl;
+            break;
+        case 4:
+            cout <<"-Celsius to Fahrenheit Converter-" << endl;
+            cout << "Enter the temperature in Celsius: " << endl;
+            cin >> tempC;
+            cout << "The temperature in Fahrenheit is: " << convertCtoF(tempC) <<endl;
         case 0:
-            cout << "You chose 0. The converter will now exit to the main menu" << endl;
             break;
         default:
             cout << "Sorry " << choice << " is not a valid selection. " << endl;
--- M5HW1_Stevens/Dataman/temperature.h
+++ M5HW1_Stevens/Dataman/temperature.h
@@ -2,7 +2,10 @@
 #define TEMPERATURE_H_INCLUDED
 
 void tempConvertMenu();
+
 float convertKtoC(float);
 float convertCtoK(float);
+float convertFtoC(float);
+float convertCtoF(float);
 
 #endif // TEMPERATURE_H_INCLUDED
